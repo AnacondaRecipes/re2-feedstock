c9adc344547cd555b6bbd44d8806daab9512e78c
Lorenzo Pirritano <lpirritano@anaconda.com>
2025-01-03 13:54:41 +0100

    refactor to consider SONAME

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 set -ex
 
-mkdir build-cmake
-pushd build-cmake
-
 if [[ "${target_platform}" == osx-* ]]; then
-    # CMAKE_CXX_STANDARD=17 does not work for osx
-    # will set -std=gnu++17 which does not work
-    # for this project
-    CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX=1 -std=c++17"
+  export CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX=1"
 fi
 
-cmake ${CMAKE_ARGS} -GNinja \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_INSTALL_LIBDIR=lib \
-  -DENABLE_TESTING=OFF \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_SHARED_LIBS=ON \
-  ..
+mkdir build
+cd build
 
-ninja -v install
+cmake -GNinja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
 
-popd
+cmake --build .
 
-# Also do this installation to get .pc files. This duplicates the compilation
-# but gets us all necessary components without patching.
-make -j "${CPU_COUNT}" prefix=${PREFIX} shared-install
+if [[ "${target_platform}" == "${build_platform}" ]]; then
+    # same test filter as upstream uses, see
+    # https://github.com/google/re2/blob/main/.github/cmake.sh
+    ctest --output-on-failure -E "dfa|exhaustive|random"
+fi
